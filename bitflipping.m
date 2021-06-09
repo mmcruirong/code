26bd8efdbb28c37ef150641d6d008a56b49e7253
Ruriong Chen <33703933+mmcruirong@users.noreply.github.com>
2021-06-09 00:33:44 -0400

    ..

--- bitflipping.m
+++ bitflipping.m
@@ -2,19 +2,19 @@
 % Ruirong Chen 
 % University of Pittsburgh
 %clear all
-bits = randi([0 1],2400,1);
-encoded_bits = wlanBCCEncode(bits,'1/2');
+bits = randi([0 1],240,1);
+encoded_bits = wlanBCCEncode(bits,'5/6');
 error_bits = encoded_bits;
 randi_loc = sort(randi([1 240],20,1));
-error_bits(80:7:2800,1) = ~encoded_bits(80:7:2800,1);
+error_bits(80:150,1) = ~encoded_bits(80:150,1);
 %error_bits(randi_loc,1) = ~encoded_bits(randi_loc,1);
-error_bits_decode = wlanBCCDecode(error_bits,'1/2','hard');
-error_bits_encode = wlanBCCEncode(error_bits_decode,'1/2');
+error_bits_decode = wlanBCCDecode(error_bits,'5/6','hard');
+error_bits_encode = wlanBCCEncode(error_bits_decode,'5/6');
 bit_difference = sum(abs(double(error_bits_decode) - double(bits)));
 flip_bits = error_bits_encode;
-flip_bits(80:7:2800,1) = ~error_bits_encode(80:7:2800,1);
-flip_bits_decode = wlanBCCDecode(flip_bits,'1/2','hard');
-flip_bits_encode = wlanBCCEncode(flip_bits_decode,'1/2');
+flip_bits(80:110,1) = ~error_bits_encode(80:110,1);
+flip_bits_decode = wlanBCCDecode(flip_bits,'5/6','hard');
+flip_bits_encode = wlanBCCEncode(flip_bits_decode,'5/6');
 
 bit_difference_flip =  sum(abs(double(flip_bits) - double(error_bits)));
 bit_difference_encode =  sum(abs(double(error_bits_encode) - double(error_bits)));
